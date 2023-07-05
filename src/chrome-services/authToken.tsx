96497c4fb30d417a4032434d99cce93c2646a745
Shaine O'Neal <shaineoneal@gmail.com>
2023-07-04 22:54:41 -0400

    feat(spreadsheet): enhance site and spreadsheet query functionality

--- src/chrome-services/authToken.tsx
+++ src/chrome-services/authToken.tsx
@@ -1,5 +1,10 @@
 import { log } from '../utils/logger';
 
+/** get token from identity API
+ * 
+ * @param interactive = true if you want to ask the user for permission to access their google account
+ * @returns token or empty string
+ */
 export function fetchToken(interactive?: boolean): Promise<string> {
     return new Promise((resolve) => {
 
@@ -19,32 +24,20 @@ export function fetchToken(interactive?: boolean): Promise<string> {
         } else {
             // get token from identity API
             chrome.identity.getAuthToken({ interactive: false }, (token) => {
-                    if (token) {
-                        resolve(token);
-                    } else {
-                        chrome.identity.clearAllCachedAuthTokens(() => {
-                            log('Cleared all cached');
-                            resolve(''); // TODO: make an actual error
-                        });
-                        }
-                    }
-                );
-        //} else {
-        //    // check for token saved in storage
-        //    chrome.storage.sync.get('authToken', (result) => {
-        //        const token = result.authToken;
-        //        if (token !== undefined) {
-        //            log('user has token: ', token);
-        //            resolve(token);
-        //        } else {
-        //            resolve('');
-        //        }
-        //    });
-        //}
+                if (token) {
+                    resolve(token);
+                } else {
+                    chrome.identity.clearAllCachedAuthTokens(() => {
+                        log('Cleared all cached');
+                        resolve(''); // TODO: make an actual error
+                    });
                 }
+            });
+        }
     });
 }
 
+//remove token from chrome storage and identity API
 export async function removeToken() {
     const token = await fetchToken();
 
@@ -55,6 +48,8 @@ export async function removeToken() {
     // remove identity token
     chrome.identity.removeCachedAuthToken({ token: token });
 }
+
+
 // Get the user's saved token from chrome storage
 export async function getSavedToken() {
     return new Promise<string>((resolve) => {
