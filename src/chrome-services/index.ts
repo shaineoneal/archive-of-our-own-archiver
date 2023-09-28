2891a28df3d856489368006212aadca0dea01b76
Shaine O'Neal <shaineoneal@gmail.com>
2023-09-27 20:26:01 -0400

    feat(sheet): update column sizing and add remove work function

--- src/chrome-services/index.ts
+++ src/chrome-services/index.ts
@@ -3,4 +3,6 @@ export * from './spreadsheet';
 export * from './utils/addWorkToSheet';
 export * from './utils/getSheetId';
 export * from './utils/cookies'
+export * from './utils/oauthSignIn';
+export * from './utils/removeWorkFromSheet';
 
