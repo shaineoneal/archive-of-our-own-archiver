77159e109b8b2a0018aa4f0f89db3d6f7b05f175
Shaine O'Neal <shaineoneal@gmail.com>
2023-09-29 21:09:16 -0400

    changed to values.append instead of batchUpdate

--- src/chrome-services/utils/addWorkToSheet.tsx
+++ src/chrome-services/utils/addWorkToSheet.tsx
@@ -68,3 +68,40 @@ export const addWorkToSheet = async (spreadsheetUrl: string, authToken: string, 
 }
 
     
+//need to use values.append so row can be attached to ao3 blurb
+export const ALTaddWorkToSheet = async (spreadsheetUrl: string, authToken: string, work: BaseWork) => {;
+    log('addWorkToSheet', work);
+
+    const sheetId: number = await getSheetId(spreadsheetUrl, authToken);
+    log('addWorkToSheet', 'sheetId', sheetId);
+
+    return fetch(
+    `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetUrl.split('/')[5]}/values/SavedWorks!A1:K1:append?valueInputOption=USER_ENTERED&insertDataOption=INSERT_ROWS`,
+    {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${authToken}`,
+        },
+        body: JSON.stringify({
+            'range': 'SavedWorks!A1:K1',
+            'majorDimension': 'ROWS',
+            'values': [
+                [
+                    work.workId,                     //0
+                    work.title,                      //1
+                    work.author.toString(),          //2   
+                    work.fandoms.toString(),        //3
+                    work.relationships.toString(), //4
+                    work.tags.toString(),              //5
+                    work.description,                   //6
+                    work.wordCount,                  //7   
+                    work.totalChapters,              //8
+                    work.status,                     //9
+                    work.rating,                     //10
+                ], 
+            ],
+        }),
+    }
+    ).then((res) => res.json());
+}
