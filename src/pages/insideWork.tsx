13672c291b268679038c9483bfdfd44196e5e708
Shaine O'Neal <shaineoneal@gmail.com>
2023-10-13 19:05:40 -0400

    refactor(chrome-services): clean up and organize service files

--- src/pages/insideWork.tsx
+++ src/pages/insideWork.tsx
@@ -4,44 +4,5 @@ import { BaseWork } from "../works/BaseWork";
 export class InsideWork extends BaseWork {
     static createWork(work: Element) {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     }
 }
