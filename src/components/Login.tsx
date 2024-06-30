105b14b46b3d37cefd8dab7ddf9b0f0875d62c70
Shaine ONeal <shaineoneal@gmail.com>
2024-06-30 16:42:15 -0400

    feat(auth): switch to new auth method and enhance documentation

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,30 +1,54 @@
 import { useContext } from 'react';
 import { fetchSpreadsheetUrl } from '../chrome-services/spreadsheet';
-import { launchWebAuthFlow } from '../chrome-services/utils/oauthSignIn';
+import { chromeLaunchWebAuthFlow, AuthFlowResponse, requestAuthorizaton } from '../chrome-services/utils/oauthSignIn';
 import { LoaderContext, TokenContext } from '../contexts';
+import log from '../utils/logger';
 
+/**
+ * Component for the login functionality.
+ * Allows users to log in to Google and obtain an access token.
+ * @category Component
+ * @group Popup
+ * @returns the LoginButton component 
+ * ```tsx
+ * <div>
+ *      <button>
+ *          Login
+ *      </button>
+ * </div>
+ * ```
+ */
 export const Login = () => {
     const { loader, setLoader } = useContext(LoaderContext);
     const { setAuthToken } = useContext(TokenContext);
 
+    /**
+     * Handles the login process.
+     * Sets the loader to true, launches the web authentication flow,
+     * requests authorization, sets the access token and refresh token (if available),
+     * fetches the spreadsheet URL, and sets the loader to false.
+     */
     const handleLogin = async () => {
         setLoader(true);
 
-        launchWebAuthFlow(true).then((cookie) => {
-            setAuthToken(cookie.value);
-        });
+        chromeLaunchWebAuthFlow().then((authFlowResponse: AuthFlowResponse) => {
+            log('authFlowResponse: ', authFlowResponse);
+            if (authFlowResponse.url && authFlowResponse.code) {
+                requestAuthorizaton(authFlowResponse).then((authRequestResponse) => {
+                    log('authRequestResponse: ', authRequestResponse);
+                    setAuthToken(authRequestResponse.access_token);
 
-        const url = await fetchSpreadsheetUrl();
-        if (url === null) {
-            throw new Error('Error getting spreadsheet url');
-        } else {
-            chrome.storage.sync.set({ spreadsheetUrl: url });
-        }
-
-        //log('loginCookie: ', getCookie('login', 'https://archiveofourown.org'));
-
-        setLoader(false);
-    };
+                    if (authRequestResponse.refresh_token) {
+                        chrome.storage.sync.set({ refresh_token: authRequestResponse.refresh_token });
+                    }
+                });
+            }
+        }).then(() => {
+            fetchSpreadsheetUrl().then((url) => {
+                chrome.storage.sync.set({ spreadsheetUrl: url });
+            });
+        }).then(() => { setLoader(false); });
+    }
 
     return (
         <>
