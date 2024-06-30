b621faf05514c545947d6d2649fc9193bdc07372
Shaine ONeal <shaineoneal@gmail.com>
2024-06-30 16:42:15 -0400

    switched over to new auth method + worked more on documentaion + added session storage

--- src/chrome-services/refreshToken.ts
+++ src/chrome-services/refreshToken.ts
@@ -1,3 +1,6 @@
+import log from "../utils/logger";
+import { getSessionStore } from "./store";
+
 /**
  * Retrieves the refresh token from Chrome storage.
  * 
@@ -8,10 +11,28 @@ export function getSyncedRefreshToken(): Promise<string> {
     return new Promise((resolve, reject) => {
         chrome.storage.sync.get(['refresh_token'], (result) => {
             if (chrome.runtime.lastError) {
+                log('Error getting refresh token: ', chrome.runtime.lastError);
                 reject(chrome.runtime.lastError);
             }
             else {
+                log('refresh token: ', result.refresh_token);
                 resolve(result.refresh_token);
+            }
+        });
+    });
+}
+
+/**
+ * Retrieves the session refresh token.
+ * @returns A promise that resolves with the session refresh token.
+ */
+export function getSessionRefreshToken(): Promise<string> {
+    return new Promise((resolve, reject) => {
+        getSessionStore('refresh_token').then((data: any) => {
+            if (data.refresh_token) {
+                resolve(data.refresh_token);
+            } else {
+                resolve('');
             }
         });
     });
