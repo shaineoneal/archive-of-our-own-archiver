d29fcf20b77e950794550955ea79e446f0296213
Shaine ONeal <shaineoneal@gmail.com>
2024-09-26 20:26:47 -0400

    chore(wiki): remove obsolete wiki directory and related files

--- src/components/popup_body.tsx
+++ src/components/popup_body.tsx
@@ -1,51 +1,51 @@
 import { useContext, useEffect, useState } from 'react';
-import { fetchNewAccessToken, fetchSpreadsheetUrl, getLocalAccessToken, isAccessTokenValid } from '../chrome-services';
+import { exchangeRefreshForAccessToken, fetchSpreadsheetUrl, getLocalAccessToken, getValidAccessToken, isAccessTokenValid, setStore, StoreMethod } from '../chrome-services';
 import { GoToSheet, Login } from '../components';
 import { LoaderContext, TokenContext } from '../contexts';
 import log from '../utils/logger';
+import { getRefreshToken, getValidRefreshToken } from '../chrome-services/refreshToken';
 
 export const PopupBody = () => {
     //begin with loader on
     const { loader, setLoader } = useContext(LoaderContext);
     const [ spreadsheetUrl, setSpreadsheetUrl ] = useState<string>('');
-    const { authToken, setAuthToken } = useContext(TokenContext);
     
+    const { authToken, setAuthToken } = useContext(TokenContext);
+
 
     useEffect(() => {
-        log('useEffect');
+        const fetchTokens = async () => {
+            // if both access and refresh token exist, use them
+            try {
+                const accessT = await getValidAccessToken();
+                const refreshT = await getValidRefreshToken();
 
-        getLocalAccessToken().then((token) => {
-            isAccessTokenValid(token).then((valid) => {
-                log('valid: ', valid);
-                if (!valid) {
-                    setAuthToken('');
-                } else {
-                    setAuthToken(valid);
+                if (accessT && refreshT) {
+                    setAuthToken(accessT);
                 }
-            });
-        }).catch(() => {
-            fetchNewAccessToken().then((newToken) => {
-                if (authToken !== newToken) setAuthToken(newToken);
-            });   
-        });
+            } catch (error) {
+                // if only refresh token exists, exchange it for an access token
+                try {
+                    const refreshT = await getValidRefreshToken();
+                    log("refreshT: ", refreshT);
+                    const response = await exchangeRefreshForAccessToken(refreshT);
+                    log("exchangeRefreshTokenForAccessToken response: ", response);
+                    if (authToken !== response) {
+                        setAuthToken(response);
+                        await setStore('accessToken', response, StoreMethod.LOCAL);
+                    }
+                } catch (error) {
+                    log("No refresh token found.", error)
+                }
+            } finally {
+                setLoader(false);
+            }
+        };
 
-        async function getUserInfo() {
-
-            log('authToken: ', authToken);
-
-            fetchSpreadsheetUrl().then((url) => {
-                log('url: ', url);
-                setSpreadsheetUrl(url);
-            });
-        }
-
-        getUserInfo().then(() => {
-            setLoader(false);
-        });
-    }, [authToken]);
+        fetchTokens();
+    }, []);
 
     if (loader) {
-        log('loader is true');
         return <div className="loader"></div>;
     } else {
         return (
@@ -56,10 +56,6 @@ export const PopupBody = () => {
             </>
         );
     }
-};88
+};
 
 export default PopupBody;
-function refreshToken() {
-    throw new Error('Function not implemented.');
-}
-
