df70e1a1a3efd5cf5d4baee7737969db582a1170
Shaine ONeal <shaineoneal@gmail.com>
2024-09-26 20:26:47 -0400

    chore(wiki): remove obsolete wiki directory and related files

--- src/chrome-services/accessToken.tsx
+++ src/chrome-services/accessToken.tsx
@@ -1,7 +1,7 @@
 import log from '../utils/logger';
-import { getLocalRefreshToken } from './refreshToken';
+import { getRefreshToken, getValidRefreshToken } from './refreshToken';
 import { getStore, StoreMethod } from './store';
-import { HttpMethod, HttpRequest, makeRequest } from './utils/httpRequest';
+import { HttpMethod, HttpRequest, HttpResponse, makeRequest } from './utils/httpRequest';
 
 const { oauth2 } = chrome.runtime.getManifest();
 const client_secret = process.env.REACT_APP_CLIENT_SECRET;
@@ -10,59 +10,44 @@ const redirectUri = chrome.identity.getRedirectURL();
 /**
  * Fetches a new access token using the OAuth2 refresh token.
  * 
- * This function checks if the OAuth2 configuration is valid, retrieves the refresh token from session storage,
+ * This function first checks if the OAuth2 configuration is valid, retrieves the refresh token from session storage,
  * and makes a POST request to the OAuth2 server to obtain a new access token.
  * 
  * @returns {Promise<string>} A promise that resolves with the new access token.
  * @throws {Error} Throws an error if the OAuth2 configuration is invalid or if there is an error getting the refresh token.
  * @group accessToken
+ * @see {@link https://www.xiegerts.com/post/chrome-extension-google-oauth-refresh-token/ | Handling Google OAuth Refresh Tokens in a Chrome Extension}
 */
-export function fetchNewAccessToken(): Promise<string> {
-
+export async function exchangeRefreshForAccessToken(refreshT: string): Promise<string> {
     if (!oauth2 || !client_secret) {
         throw new Error('Invalid oauth2 configuration');
     }
 
-    return new Promise((resolve, reject) => {
-        //get refresh token from session storage
-        getLocalRefreshToken().then(async (refreshToken) => {
-            if (refreshToken === '') {
-                log('Error getting refresh token');
-                reject('Error getting refresh token');
-            }
-            const options = {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                },
-                body: new URLSearchParams({
-                    client_id: oauth2.client_id,
-                    client_secret: client_secret,
-                    refresh_token: refreshToken,
-                    grant_type: 'refresh_token',
-                }),
-            };
-            //const request: HttpRequest = {
-            //    url: 'https://oauth2.googleapis.com/token',
-            //    method: HttpMethod.POST,
-            //    headers: {
-            //        'Content-Type': 'application/x-www-form-urlencoded',
-            //    },
-            //    body: {
-            //        client_id: oauth2.client_id,
-            //        client_secret: client_secret,
-            //        refresh_token: refreshToken,
-            //        grant_type: 'refresh_token',
-            //    },
-            //};
+    if (refreshT === undefined) {
+        throw new Error('Error getting refresh token');
+    }
 
-            fetch("https://oauth2.googleapis.com/token", options).then(async (response) => {
-                const parsedResponse = await response.json();
-                log('fetchNewAccessToken Response: ', parsedResponse);
-                resolve(parsedResponse.access_token);
-            });
-        });
+    const response = await makeRequest({
+        url: 'https://oauth2.googleapis.com/token',
+        method: HttpMethod.POST,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: {
+            client_id: oauth2.client_id,
+            client_secret: client_secret,
+            refresh_token: refreshT,
+            grant_type: 'refresh_token',
+        },
     });
+
+    if (!response.ok) {
+        throw new Error(response.statusText);
+    }
+
+    const parsedResponse = await response.json();
+    log('fetchNewAccessToken Response: ', parsedResponse);
+    return parsedResponse.access_token;
 }
 
 //remove token from chrome storage and identity API
@@ -115,7 +100,6 @@ export async function getLocalAccessToken(): Promise<string> {
 export async function isAccessTokenValid(token: string): Promise<string> {
     
     return new Promise(async (resolve, reject) => {
-        log('isAccessTokenValid', token);
         if (token != '') {
             makeRequest({
                 url: 'https://oauth2.googleapis.com/tokeninfo?access_token=' + token,
@@ -139,3 +123,34 @@ export async function isAccessTokenValid(token: string): Promise<string> {
         }
     });
 }
+
+
+/**
+ * Checks if the access token is valid by first checking local storage and then sync storage for the token.
+ *
+ * @returns {Promise<string>} A Promise that resolves with the valid access token or rejects if the token is invalid.
+ * @throws {Error} Throws an error if no token is found.
+ * @async
+ * @group accessToken
+ */
+export async function getValidAccessToken(): Promise<string> {
+    try {
+        log('getValidAccessToken');
+        // Get the access token from local storage
+        let token = await getLocalAccessToken();
+        // If the token is not undefined, check if it is valid
+        if (token !== undefined) {
+            await isAccessTokenValid(token);
+            // If the token is valid, return it
+            return token;
+        } else {
+            // If no token is found, throw an error
+            throw new Error('No token found');
+        }
+    } catch (error) {
+        // If there is an error, log it and rethrow the error
+        log('Token is invalid: ', error);
+        throw error;
+    }
+}
+
