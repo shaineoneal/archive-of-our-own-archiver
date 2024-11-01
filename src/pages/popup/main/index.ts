34df5d74f33c58de741e7afea928db165c362c99
Shaine ONeal <shaineoneal@gmail.com>
2024-11-01 17:19:43 -0400

    folder restructuring done and working

--- src/pages/popup/main/index.ts
+++ src/pages/popup/main/index.ts
@@ -1,5 +1,3 @@
-/*************  ✨ Codeium Command ⭐  *************/
 export * from './GoToSheet';
 export * from './Login';
 export * from './popup_body';
-/******  ff47fdc2-bced-4086-81a8-0a69ca99428b  *******/
