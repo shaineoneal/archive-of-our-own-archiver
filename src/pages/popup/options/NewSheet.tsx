ce07bc17ebc8b64ced89da0020e3e8d7ff6bcd88
Shaine ONeal <shaineoneal@gmail.com>
2024-11-07 09:34:52 -0500

    fix(auth): resolve logout errors and ensure token deletion

--- src/pages/popup/options/NewSheet.tsx
+++ src/pages/popup/options/NewSheet.tsx
@@ -1,16 +1,36 @@
-import { getLocalAccessToken, createSpreadsheet } from '../../../utils/chrome-services';
-import { useLoaderStore } from '../../../utils/zustand';
+import { createSpreadsheet } from '../../../utils/chrome-services';
+import { useLoaderStore, useUser } from '../../../utils/zustand';
 
+/**
+ * Component for creating a new Google Sheet.
+ * Allows users to create a new spreadsheet and store its URL in Chrome's sync storage.
+ * Displays a loader while the spreadsheet is being created.
+ * @component
+ * @group Popup
+ * @returns the NewSheet component
+ */
 export const NewSheet = () => {
     const { loader, setLoader } = useLoaderStore();
+    const accessToken = useUser().accessToken;
 
+    if (accessToken === undefined) {
+        throw new Error('User is not logged in');
+    }
+
+    /**
+     * Handles the creation of a new Google Sheet.
+     * Sets the loader state to true while the spreadsheet is being created.
+     * Stores the spreadsheet URL in Chrome's sync storage.
+     */
     const handleNewSheet = async () => {
         setLoader(true);
-        createSpreadsheet(await getLocalAccessToken())
+        createSpreadsheet(accessToken)
         .then((url) => {
             chrome.storage.sync.set({ spreadsheetUrl: url });
         })
-
+        .finally(() => {
+            setLoader(false);
+        });
     };
 
     if (loader) {
@@ -23,4 +43,3 @@ export const NewSheet = () => {
         </div>
     );
 };
-export default NewSheet;
