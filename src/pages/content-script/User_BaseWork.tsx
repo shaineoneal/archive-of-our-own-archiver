66ec454d3b4bb1c35176ee466426f91116b0cee1
Shaine ONeal <shaineoneal@gmail.com>
2024-11-12 15:30:02 -0500

    changed BaseWork structure into Ao3_BaseWork and User_BaseWork

---
+++ src/pages/content-script/User_BaseWork.tsx
@@ -0,0 +1,39 @@
+import { WORK_STATUSES, WorkStatus } from "../../utils/types/data";
+import { Ao3_BaseWork } from "./Ao3_BaseWork";
+import { BaseWork } from "./BaseWork";
+import { querySpreadsheet } from "../../utils/chrome-services";
+
+export class User_BaseWork extends BaseWork {
+    status: WorkStatus;
+    history: string;        //TODO: change to array of updates
+    personalTags?: string[];
+    rating: number;
+    readCount: number;
+    skipReason?: string;
+
+    constructor(workId: number, status: WorkStatus, history: string, personalTags: string[], rating: number, readCount: number, skipReason: string) {
+        super(workId);
+        this.status = status ?? WORK_STATUSES["read"];
+        this.history = history ?? '';
+        this.personalTags = personalTags ?? [];
+        this.rating = rating ?? 0;
+        this.readCount = readCount ?? 1;
+        this.skipReason = skipReason ?? '';
+    }
+
+    getWork(workId: number): User_BaseWork {
+        return new User_BaseWork(
+            workId,
+            WORK_STATUSES["read"],
+            '',
+            [],
+            0,
+            0,
+            ''
+        );
+
+    }
+    saveWorkToSession(work: User_BaseWork) {
+
+    }
+}
