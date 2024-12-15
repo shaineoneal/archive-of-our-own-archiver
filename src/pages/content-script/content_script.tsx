a7aae3e4ef67c608e4af88774423bfcbce4b4ea4
Shaine ONeal <shaineoneal@gmail.com>
2024-12-15 12:19:01 -0500

    Removed commented code

--- src/pages/content-script/content_script.tsx
+++ src/pages/content-script/content_script.tsx
@@ -2,9 +2,7 @@ import { standardBlurbsPage } from '..';
 import log from '../../utils/logger';
 import { initializePort, MessageName, sendMessage } from "../../utils/chrome-services/messaging";
 
-
 log('log: content_script.tsx loaded');
-
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     log('content_script', 'heard message: ', message);
@@ -14,51 +12,7 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     }
 });
 
-/*
- * @startuml
- * participant content_script [[content_script.tsx]]
- * participant background [[../background/background.ts]]
- * participant "session storage" as session
- *
- * content_script -> background : connectPort
- * activate background
- * background --> content_script : port
- * deactivate background
- *
- * activate content_script
- * content_script -> background : checkLogin
- * activate background
- * background -> background : check login
- * activate background #green
- * background -> session : set user
- * activate session
- * session --> background : user set
- * deactivate session
- * background --> content_script : loggedIn
- * @enduml
- */
 initializePort()
-//    .then((port) => {
-//    log('connected to port');
-//
-//    //port.postMessage({ message: 'checkLogin' });
-//    sendMessage(port, MessageName.CheckLogin, {})
-//
-//    //receiveMessage(port, 'querySpreadsheet', async (payload) => {
-//    //    log('querySpreadsheet payload: ', payload);
-//    //    return true;
-//    //});
-//    port.onMessage.addListener((msg) => {
-//        log('content_script', 'port.onMessage: ', msg);
-//        if (msg.payload === true) {
-//            log('user is logged in');
-//            pageTypeDetect(port);
-//        }
-//        else if (!msg.loggedIn) {
-//            log('user is not logged in');
-//        }
-//    });
-//});
 
 sendMessage(
     MessageName.CheckLogin,
@@ -73,13 +27,6 @@ sendMessage(
         }
     }
 )
-
-//open up connection to background script
-async function connectPort(): Promise<chrome.runtime.Port> {
-    const port = chrome.runtime.connect({ name: 'content_script' });
-    log('port: ', port);
-    return port;
-}
 
 //TODO: check for work v. bookmark page first
 
