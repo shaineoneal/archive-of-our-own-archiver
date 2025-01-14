270b08b428e142c49a68edd0af5c18c030a19162
Shaine ONeal <shaineoneal@gmail.com>
2025-01-13 19:33:51 -0500

    added index to sheet

--- src/pages/background/background.ts
+++ src/pages/background/background.ts
@@ -81,20 +81,20 @@ createMessageHandlers({
             forEach(response.table.rows, (row) => {
                 log('row', row);
 
-                let status = row.c[1] ? row.c[1].v : 'read';
-                let history = row.c[2] ? row.c[2].v : '';
-                let personalTags = row.c[3] ? row.c[3].v.split(',') : [];
-                let rating = row.c[4] ? row.c[4].v : 0;
-                let readCount = row.c[5] ? row.c[5].v : 1;
-                let skipReason = row.c[6] ? row.c[6].v : '';
+                let index = row.c[0].v ? row.c[0].v : 0;
+                let status = row.c[2] ? row.c[2].v : 'read';
+                let history = row.c[3] ? row.c[3].v : '';
+                let personalTags = row.c[4] ? row.c[4].v.split(',') : [];
+                let rating = row.c[5] ? row.c[5].v : 0;
+                let readCount = row.c[6] ? row.c[6].v : 1;
+                let skipReason = row.c[7] ? row.c[7].v : '';
 
-                let work = new User_BaseWork(row.c[0].v, status, history, personalTags, rating, readCount, skipReason);
+                let work = new User_BaseWork(index, row.c[1].v, status, history, personalTags, rating, readCount, skipReason);
 
                 chrome.storage.session.set({[row.c[0].v]: work}).then(r =>
                     log('set session storage', r)
                 );
             });
-
             responseArray = compareArrays(msg.list, response.table.rows);
             log('responseArray', responseArray);
             log('responseArray type', typeof responseArray);
