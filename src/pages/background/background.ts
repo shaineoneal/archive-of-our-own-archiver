5dc0a9264944a58b4100b48312cd8886f87d1fac
Shaine ONeal <shaineoneal@gmail.com>
2025-01-27 16:24:51 -0500

    fix: random, im tired of committing

--- src/pages/background/background.ts
+++ src/pages/background/background.ts
@@ -28,17 +28,17 @@ chrome.runtime.onConnect.addListener((port) => {
 
 createMessageHandlers({
     // Called when the popup is opened
-    [MessageName.CheckLogin]: async () => {
+    [MessageName.CheckLogin]: async (): Promise<MessageResponse<{ status: boolean }>> => {
         log('checkLogin message received');
         const { setAccessToken } = SyncUserStore.getState().actions;
         let syncUser = SyncUserStore.getState().user;
         log('syncUser', syncUser);
-        if(syncUser.isLoggedIn && syncUser.accessToken) {
+        if (syncUser.isLoggedIn && syncUser.accessToken) {
             log('user is logged in');
             setStore('user', syncUser, StoreMethod.SESSION);
             const isValid = await isAccessTokenValid(syncUser.accessToken);
             log('is access token valid', isValid);
-            if(!isValid) {
+            if (!isValid) {
                 log('access token is not valid');
                 if (syncUser.refreshToken) {
                     try {
@@ -48,100 +48,38 @@ createMessageHandlers({
                             setAccessToken(newAccessToken);
                             await setAccessTokenCookie(newAccessToken);
                             log('newAccessToken set');
+                            return { response: { status: true } };
                         } else {
                             log('Error exchanging refresh token for access token');
+                            return { response: { status: false } };
                         }
                     } catch (error) {
                         log('Error exchanging refresh token for access token', error);
+                        return { response: { status: false } };
                     }
+                } else {
+                    return { response: { status: false } };
                 }
             } else {
                 setAccessToken(isValid);
                 await setAccessTokenCookie(isValid);
                 log('newAccessToken set');
+                return { response: { status: true } };
             }
-            return { status: true } ;
-        } else return { status: false } ;
-    },
-    [MessageName.QuerySpreadsheet]: async (msg) => {
-        let syncUser = SyncUserStore.getState().user;
-        const { setAccessToken } = SessionUserStore.getState().actions;
-
-        if (syncUser.spreadsheetId === undefined || syncUser.accessToken === undefined) {
-            throw new Error('no spreadsheetId or accessToken');
+        } else {
+            return { response: { status: false } };
         }
-
-        //let syncUser = SyncUserStore.getState().user;
-        log('querySheet message received');
-        let responseArray: boolean[] = [];
-        return querySpreadsheet(syncUser.spreadsheetId, syncUser.accessToken, msg.list).then((response) => {
-            log('querySheet response', response);
-            log('querySheet # rows', response.table.rows.length);
-            if (response.table.rows.length == 0) {
-                log('no rows');
-                //TODO: clean this mess
-                if (syncUser.refreshToken != null) {
-                    exchangeRefreshForAccessToken(syncUser.refreshToken).then((newAccessToken) => {
-                        log('newAccessToken', newAccessToken);
-                        if (newAccessToken) {
-                            chrome.storage.session.set({accessToken: newAccessToken}).then(() => {
-                                log('newAccessToken set');
-                                setAccessToken(newAccessToken);
-
-                                if (syncUser.spreadsheetId != null) {
-                                    querySpreadsheet(syncUser.spreadsheetId, newAccessToken, msg.list).then((response) => {
-                                        log('querySheet response', response);
-                                        const responseArray = compareArrays(msg.list, response.table.rows);
-                                        log('responseArray', responseArray);
-                                        return responseArray;
-
-                                    });
-                                }
-                            });
-                        }
-                    });
-                }
-                return response.error;
-            }
-
-            forEach(response.table.rows, (row) => {
-                log('row', row);
-
-                let index = row.c[0].v ? row.c[0].v : 0;
-                let status = row.c[2] ? row.c[2].v : 'read';
-                let history = row.c[3] ? row.c[3].v : '';
-                let personalTags = row.c[4] ? row.c[4].v.split(',') : [];
-                let rating = row.c[5] ? row.c[5].v : 0;
-                let readCount = row.c[6] ? row.c[6].v : 1;
-                let skipReason = row.c[7] ? row.c[7].v : '';
-
-                let work = new User_BaseWork(row.c[1].v, index, status, history, personalTags, rating, readCount, skipReason);
-
-                chrome.storage.session.set({[row.c[1].v]: work}).then(r =>
-                    log('set session storage', r)
-                );
-            });
-            responseArray = compareArrays(msg.list, response.table.rows);
-            log('responseArray', responseArray);
-            log('responseArray type', typeof responseArray);
-            return responseArray;
-        }).catch((error) => {
-            throw new Error(error);
-        });
     },
-    [MessageName.AddWorkToSheet]: async (payload) => {
+    [MessageName.AddWorkToSheet]: async (payload): Promise<MessageResponse<User_BaseWork>> => {
         let sessionUser = SyncUserStore.getState().user;
         log('payload', payload);
         if (sessionUser.spreadsheetId !== undefined && sessionUser.accessToken !== undefined) {
-            const work =  await addWorkToSheet(sessionUser.spreadsheetId, sessionUser.accessToken, payload.work);
+            const work = await addWorkToSheet(sessionUser.spreadsheetId, sessionUser.accessToken, payload.work);
             setStore(`${payload.work.workId}`, work, StoreMethod.SESSION);
-            return work;
-
+            return { response: work };
         } else {
-            log(payload)
-            log('sessionUser', sessionUser);
+            log(payload);
             throw new Error('no spreadsheetId or accessToken');
-            //TODO: handle this better
         }
     },
     [MessageName.RemoveWorkFromSheet]: async (payload): Promise<MessageResponse<boolean>> => {
@@ -167,10 +105,10 @@ createMessageHandlers({
             return { response: false };
         }
     },
-    [MessageName.RefreshAccessToken]: async () => {
+    [MessageName.RefreshAccessToken]: async (): Promise<MessageResponse<string>> => {
         let syncUser = SyncUserStore.getState().user;
         const { setAccessToken } = SyncUserStore.getState().actions;
-        log('refreshAccessToken message received');
+        log('refreshAccessToken message received', 'syncUser', syncUser);
         if (syncUser.refreshToken) {
             try {
                 const accessToken = await exchangeRefreshForAccessToken(syncUser.refreshToken);
@@ -181,75 +119,75 @@ createMessageHandlers({
                         await setAccessTokenCookie(accessToken);
                         setAccessToken(accessToken);
                     }
-                    return accessToken;
+                    return { response: accessToken };
+                } else {
+
+                    return { response: '' };
                 }
-                else return 'testing';
             } catch (error) {
-                log('Error exchanging refresh token for access token', error);
-                return '';
+                log('Error exchanging refresh token for access token\n', error);
+                return { response: '' };
             }
-        } else return '';
+        } else {
+            return { response: '' };
+        }
     },
-    [MessageName.UpdateWorkInSheet]: async (payload) => {
+    [MessageName.UpdateWorkInSheet]: async (payload): Promise<MessageResponse<boolean>> => {
         log('payload', payload);
         let sessionUser = SyncUserStore.getState().user;
         log('sessionUser', sessionUser);
         if (sessionUser.spreadsheetId !== undefined && sessionUser.accessToken !== undefined) {
-            const resp =  await updateWorkInSheet(sessionUser.spreadsheetId, sessionUser.accessToken, payload.work);
-            if(resp) {
+            const resp = await updateWorkInSheet(sessionUser.spreadsheetId, sessionUser.accessToken, payload.work);
+            if (resp) {
                 log('work updated in sheet');
                 setStore(`${payload.work.workId}`, payload.work, StoreMethod.SESSION);
-                return true;
+                return { response: true };
             } else {
                 log('work not updated in sheet');
-                return false;
+                return { response: false };
             }
-        } else return false;
+        } else {
+            log(payload);
+            log('sessionUser', sessionUser);
+            return { response: false };
+        }
+    },
+    [MessageName.QuerySpreadsheet]: async (msg): Promise<MessageResponse<boolean[]>> => {
+        let syncUser = SyncUserStore.getState().user;
+        const { setAccessToken } = SessionUserStore.getState().actions;
 
-    }
-});
+        if (syncUser.spreadsheetId === undefined || syncUser.accessToken === undefined) {
+            throw new Error('no spreadsheetId or accessToken');
+        }
 
-//TODO: this forces a lot of reloads, make calls to refresh the page instead
-chrome.storage.onChanged.addListener((changes) => {
-    log('storage changed', changes);
-    if( changes['user-store'] ) {
-        log('user changed', changes['user-store']);
+        log('querySheet message received');
+        let responseArray: boolean[] = [];
+        return querySpreadsheet(syncUser.spreadsheetId, syncUser.accessToken, msg.list).then((response) => {
+            log('querySheet response', response);
+            log('response.table.rows', response.table.rows);
 
-        chrome.tabs.query({ url: "*://*.archiveofourown.org/*" }, (tabs) => {
-            tabs.forEach((tab) => {
-                log('sending message to tab', tab);
-                chrome.tabs.sendMessage(tab.id!, {message: "userChanged", newUser: changes['user-store'].newValue})
-                    .then((response) => {
-                        chrome.runtime.reload();
-                        chrome.tabs.reload(tab.id!).then(r => log('reloaded tab', r));
-                        log('response from content script', response)
-                    });
+            //save the response to the session store
+            for (let row of response.table.rows) {
+                let index = row.c[0].v ? row.c[0].v : 0;
+                let status = row.c[2] ? row.c[2].v : 'read';
+                let history = row.c[3] ? row.c[3].v : '';
+                let personalTags = row.c[4] ? row.c[4].v.split(',') : [];
+                let rating = row.c[5] ? row.c[5].v : 0;
+                let readCount = row.c[6] ? row.c[6].v : 1;
+                let skipReason = row.c[7] ? row.c[7].v : '';
 
-            });
+                let work = new User_BaseWork(row.c[1].v, index, status, history, personalTags, rating, readCount, skipReason);
+
+                chrome.storage.session.set({[row.c[1].v]: work}).then(r =>
+                    log('set session storage', r)
+                );
+            }
+
+            responseArray = compareArrays(msg.list, response.table.rows);
+            log('responseArray', responseArray);
+            return { response: responseArray };
+        }).catch((error) => {
+            throw new Error(error);
         });
     }
-});
-
-chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
-    log('background', 'heard message: ', message);
-    let syncUser = SyncUserStore.getState().user;
-    const {setAccessToken} = SyncUserStore.getState().actions;
-    log('refreshAccessToken message received');
-    if (syncUser.refreshToken) {
-        try {
-            const accessToken = await exchangeRefreshForAccessToken(syncUser.refreshToken);
-            log('accessToken', accessToken);
-            if (accessToken) {
-                if (await isAccessTokenValid(accessToken)) {
-                    log('accessToken is valid');
-                    await setAccessTokenCookie(accessToken);
-                    setAccessToken(accessToken);
-                }
-                sendResponse(accessToken);
-            } else return 'testing';
-        } catch (error) {
-            log('Error exchanging refresh token for access token', error);
-            return '';
-        }
-    } else return '';
 });
