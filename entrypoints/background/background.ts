b044f608342938d8584b79cda2b1e83bfc742c91
Shaine ONeal <shaineoneal@gmail.com>
2025-02-08 18:19:43 -0500

    merge(dev): integrate changes from remote dev branch

---
+++ entrypoints/background/background.ts
@@ -0,0 +1,218 @@
+import { log } from '@/utils/logger.ts';
+import { SessionUserStore, SyncUserStore } from "@/utils/zustand";
+import {
+    addWorkToSheet,
+    createMessageHandlers,
+    exchangeRefreshForAccessToken, getStore, getValidAccessToken, isAccessTokenValid,
+    MessageName,
+    querySpreadsheet, removeStore,
+    setStore,
+    StoreMethod,
+} from "@/utils/chrome-services";
+import { compareArrays } from "@/utils/compareArrays.ts";
+import { User_BaseWork } from "../content/User_BaseWork.tsx";
+import { MessageResponse } from "@/utils/types/MessageResponse"; // Ensure this import is present
+import session = chrome.storage.session;
+import { removeWorkFromSheet } from "@/utils/chrome-services/removeWorkFromSheet.ts";
+import { updateWorkInSheet } from "@/utils/chrome-services/updateWorkInSheet.tsx";
+import { setAccessTokenCookie } from "@/utils/chrome-services/cookies.ts";
+
+let syncUser = SyncUserStore.getState().user;
+
+chrome.runtime.onConnect.addListener((port) => {
+
+});
+
+async function handleTokenExchange<T>(refreshToken: string): Promise<MessageResponse<T>> {
+    const { setAccessToken } = SyncUserStore.getState().actions;
+    try {
+        const newAccessToken = await exchangeRefreshForAccessToken(refreshToken);
+        log('newAccessToken', newAccessToken);
+        if (newAccessToken) {
+            setAccessToken(newAccessToken);
+            await setAccessTokenCookie(newAccessToken);
+            log('newAccessToken set');
+            return { response: true as unknown as T };
+        } else {
+            log('Error exchanging refresh token for access token');
+            return { response: false as unknown as T };
+        }
+    } catch (error) {
+        log('Error exchanging refresh token for access token', error);
+        return { response: false as unknown as T };
+    }
+}
+
+createMessageHandlers({
+    // Called when the popup is opened
+    [MessageName.CheckLogin]: async (): Promise<MessageResponse<boolean>> => {
+        log('checkLogin message received');
+        const { setAccessToken } = SyncUserStore.getState().actions;
+        log('syncUser', syncUser);
+        if (syncUser.isLoggedIn && syncUser.refreshToken) {
+            log('user is logged in');
+            //setStore('user', syncUser, StoreMethod.SYNC);
+            try {
+                if(syncUser.accessToken === undefined) {
+                    log('no access token');
+                    return await handleTokenExchange<boolean>(syncUser.refreshToken);
+                }
+                const isValid = await isAccessTokenValid(syncUser.accessToken);
+                log('is access token valid', isValid);
+                if(isValid) {
+                    setAccessToken(syncUser.accessToken);
+                    await setAccessTokenCookie(syncUser.accessToken);
+                    log('newAccessToken set');
+                    return { response: true };
+                } else {
+                    log('access token is not valid');
+                    return await handleTokenExchange<boolean>(syncUser.refreshToken);
+                }
+
+            } catch (error) {
+                log(error);
+                return { response: false };
+            }
+        } else {
+            log('user is not logged in');
+            return { response: false };
+        }
+    },
+    [MessageName.AddWorkToSheet]: async (payload): Promise<MessageResponse<User_BaseWork>> => {
+        let sessionUser = SyncUserStore.getState().user;
+        log('payload', payload);
+        if (sessionUser.spreadsheetId !== undefined && sessionUser.accessToken !== undefined) {
+            try {
+                const work = await addWorkToSheet(sessionUser.spreadsheetId, sessionUser.accessToken, payload.work);
+                setStore(`${payload.work.workId}`, work, StoreMethod.SESSION);
+                return { response: work };
+            } catch (error) {
+                log('error adding work to sheet', error);
+                // if user has a refresh token, try to get a new access token
+                if (sessionUser.refreshToken) {
+                    let accessT = await getValidAccessToken(sessionUser.accessToken, sessionUser.refreshToken);
+                    if (accessT) {
+                        sessionUser.accessToken = accessT;
+                        setStore('user', sessionUser, StoreMethod.SYNC);
+                        return await handleTokenExchange<User_BaseWork>(sessionUser.refreshToken);
+                    } 
+                }
+                throw new Error('access token expired or invalid, and there was an error exchanging the refresh token');
+            }
+        } else {
+            log(payload);
+            throw new Error('no spreadsheetId or accessToken');
+        }
+    },
+    [MessageName.RemoveWorkFromSheet]: async (payload): Promise<MessageResponse<boolean>> => {
+        log('payload', payload);
+        let sessionUser = SyncUserStore.getState().user;
+        const workId = `${payload.workId}`;
+        log('workId', workId);
+        const work = await getStore(workId, StoreMethod.SESSION);
+        const workIndex = work[workId].index;
+        log('workIndex', workIndex);
+        if (sessionUser.spreadsheetId !== undefined && sessionUser.accessToken !== undefined) {
+            const resp = await removeWorkFromSheet(sessionUser.spreadsheetId, sessionUser.accessToken, workIndex);
+            if (resp) {
+                log('work removed from sheet');
+                removeStore(workId, StoreMethod.SESSION);
+                return { response: true };
+            } else {
+                return { response: false };
+            }
+        } else {
+            log(payload);
+            log('sessionUser', sessionUser);
+            return { response: false };
+        }
+    },
+    [MessageName.RefreshAccessToken]: async (): Promise<MessageResponse<string>> => {
+        let syncUser = SyncUserStore.getState().user;
+        log('refreshAccessToken message received', 'syncUser', syncUser);
+        if (syncUser.refreshToken) {
+            return await handleTokenExchange<string>(syncUser.refreshToken);
+        } else { 
+            return { response: '' };
+        }
+    },
+    [MessageName.UpdateWorkInSheet]: async (payload): Promise<MessageResponse<boolean>> => {
+        log('payload', payload);
+        let sessionUser = SyncUserStore.getState().user;
+        log('sessionUser', sessionUser);
+        if (sessionUser.spreadsheetId !== undefined && sessionUser.accessToken !== undefined) {
+            const resp = await updateWorkInSheet(sessionUser.spreadsheetId, sessionUser.accessToken, payload.work);
+            if (resp) {
+                log('work updated in sheet');
+                setStore(`${payload.work.workId}`, payload.work, StoreMethod.SESSION);
+                return { response: true };
+            } else {
+                log('work not updated in sheet');
+                return { response: false };
+            }
+        } else {
+            log(payload);
+            log('sessionUser', sessionUser);
+            return { response: false };
+        }
+    },
+    [MessageName.QuerySpreadsheet]: async (msg): Promise<MessageResponse<boolean[]>> => {
+        let syncUser = SyncUserStore.getState().user;
+        const { setAccessToken } = SessionUserStore.getState().actions;
+
+        if (syncUser.spreadsheetId === undefined || syncUser.accessToken === undefined) {
+            throw new Error('no spreadsheetId or accessToken');
+        }
+
+        log('querySheet message received');
+        let responseArray: boolean[] = [];
+        return querySpreadsheet(syncUser.spreadsheetId, syncUser.accessToken, msg.list).then((response) => {
+            log('querySheet response', response);
+            log('response.table.rows', response.table.rows);
+
+            //save the response to the session store
+            for (let row of response.table.rows) {
+                let index = row.c[0].v ? row.c[0].v : 0;
+                let status = row.c[2] ? row.c[2].v : 'read';
+                let history = row.c[3] ? row.c[3].v : '';
+                let personalTags = row.c[4] ? row.c[4].v.split(',') : [];
+                let rating = row.c[5] ? row.c[5].v : 0;
+                let readCount = row.c[6] ? row.c[6].v : 1;
+                let skipReason = row.c[7] ? row.c[7].v : '';
+
+                let work = new User_BaseWork(row.c[1].v, index, status, history, personalTags, rating, readCount, skipReason);
+
+                chrome.storage.session.set({[row.c[1].v]: work}).then(r =>
+                    log('set session storage', r)
+                );
+            }
+
+            responseArray = compareArrays(msg.list, response.table.rows);
+            log('responseArray', responseArray);
+            return { response: responseArray };
+        }).catch((error) => {
+            throw new Error(error);
+        });
+    }
+});
+
+// If storage updates, update the user store on any AO3 page and refresh the stored access token
+chrome.storage.onChanged.addListener((changes, area) => {
+    log('storage changed', changes, area);
+    if (area === 'sync') {
+        log('syncUser', syncUser);
+        if (changes['user-store']) {
+            let newUser = changes['user-store'].newValue.user;
+            log('newUser', newUser);
+            if (newUser !== syncUser) {
+                log('new user', newUser);
+                SyncUserStore.getState().actions.userStoreLogin(newUser.accessToken, newUser.refreshToken);
+                setAccessTokenCookie(newUser.accessToken);
+            }
+            if (newUser.refreshToken !== syncUser.refreshToken) {
+                log('new refresh token', newUser.refreshToken);
+                SyncUserStore.getState().actions.setRefreshToken(newUser.refreshToken);
+            }
+        }
+    }
+});
---
+++ utils/chrome-services/spreadsheet.tsx
@@ -0,0 +1,373 @@
+import { log } from '@/utils/logger.ts';
+
+export async function createSpreadsheet(token: string) {
+
+    const url = 'https://sheets.googleapis.com/v4/spreadsheets';
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: 'Bearer ' + token,
+        },
+        body: JSON.stringify(sheetLayout),
+    };
+
+    log ('options: ', options);
+
+    return fetch(url, options)      //TODO: change to makeRequest
+        .then((response) => {
+            log('Response status:', response.status);
+            return response.json();
+        })
+        .then((data) => {
+            log('Success:', data);
+            return data.spreadsheetId;
+        })
+        .catch((error) => {
+            log('Error creating spreadsheet:', error);
+            throw error;
+        });
+}
+
+const sheetLayout = {
+    properties: { title: 'AO3E' },
+    namedRanges: [
+        {
+            namedRangeId: '1',
+            name: 'index',
+            range: {
+                sheetId: 0,
+                startColumnIndex: 0,
+                endColumnIndex: 1
+            }
+        },
+        {
+            namedRangeId: '2',
+            name: 'WorkID',
+            range: {
+                sheetId: 0,
+                startColumnIndex: 1,
+                endColumnIndex: 2
+            }
+        },
+        {
+            namedRangeId: '3',
+            name: 'title',
+            range: {
+                sheetId: 0,
+                startColumnIndex: 2,
+                endColumnIndex: 3
+            }
+        },
+        {
+            namedRangeId: '4',
+            name: 'authors',
+            range: {
+                sheetId: 0,
+                startColumnIndex: 3,
+                endColumnIndex: 4
+            }
+        },
+        {
+            namedRangeId: '5',
+            name: 'fandoms',
+            range: {
+                sheetId: 0,
+                startColumnIndex: 4,
+                endColumnIndex: 5
+            }
+        },
+        {
+            namedRangeId: '6',
+            name: 'relationships',
+            range: {
+                sheetId: 0,
+                startColumnIndex: 5,
+                endColumnIndex: 6
+            }
+        },
+        {
+            namedRangeId: '7',
+            name: 'tags',
+            range: {
+                sheetId: 0,
+                startColumnIndex: 6,
+                endColumnIndex: 7
+            }
+        },
+        {
+            namedRangeId: '8',
+            name: 'description',
+            range: {
+                sheetId: 0,
+                startColumnIndex: 7,
+                endColumnIndex: 8
+            }
+        },
+        {
+            namedRangeId: '9',
+            name: 'wordCount',
+            range: {
+                sheetId: 0,
+                startColumnIndex: 8,
+                endColumnIndex: 9
+            }
+        },
+        {
+            namedRangeId: '10',
+            name: 'chapterCount',
+            range: {
+                sheetId: 0,
+                startColumnIndex: 9,
+                endColumnIndex: 10
+            }
+        },
+        {
+            namedRangeId: '11',
+            name: 'status',
+            range: {
+                sheetId: 0,
+                startColumnIndex: 10,
+                endColumnIndex: 11
+            }
+        },
+        {
+            namedRangeId: '12',
+            name: 'history',
+            range: {
+                sheetId: 0,
+                startColumnIndex: 11,
+                endColumnIndex: 12
+            }
+        },
+        {
+            namedRangeId: '13',
+            name: 'personalTags',
+            range: {
+                sheetId: 0,
+                startColumnIndex: 12,
+                endColumnIndex: 13
+            }
+        },
+        {
+            namedRangeId: '14',
+            name: 'rating',
+            range: {
+                sheetId: 0,
+                startColumnIndex: 13,
+                endColumnIndex: 14
+            }
+        },
+        {
+            namedRangeId: '15',
+            name: 'readCount',
+            range: {
+                sheetId: 0,
+                startColumnIndex: 14,
+                endColumnIndex: 15
+            }
+        },
+        {
+            namedRangeId: '16',
+            name: 'skipReason',
+            range: {
+                sheetId: 0,
+                startColumnIndex: 15,
+                endColumnIndex: 16
+            }
+        }
+
+    ],
+    sheets: {
+        properties: {
+            title: 'AccessWorks',
+            sheetId: 0,
+            gridProperties: {
+                rowCount: 2,
+                columnCount: 16,
+                columnGroupControlAfter: true,
+            }
+        },
+        protectedRanges: [
+            {
+                protectedRangeId: 0,
+                range: {},
+                description: 'Protected',
+                warningOnly: true,
+            },
+        ],        
+        data: [
+            {
+                startRow: 0,
+                startColumn: 0,
+                rowData:  [
+                    {
+                        values: [
+                            {
+                                userEnteredValue: {
+                                    stringValue: 'Index',
+                                },
+                                userEnteredFormat: {
+                                    textFormat: { bold: true },
+                                },
+                            },
+                            {
+                                userEnteredValue: {
+                                    stringValue: 'Work ID',
+                                },
+                                userEnteredFormat: {
+                                    textFormat: { bold: true },
+                                },
+                            },
+                            {
+                                userEnteredValue: { stringValue: 'Title' },
+                                userEnteredFormat: {
+                                    textFormat: { bold: true },
+                                },
+                            },
+                            {
+                                userEnteredValue: {
+                                    stringValue: 'Authors',
+                                },
+                                userEnteredFormat: {
+                                    textFormat: { bold: true },
+                                },
+                            },
+                            {
+                                userEnteredValue: {
+                                    stringValue: 'Fandoms',
+                                },
+                                userEnteredFormat: {
+                                    textFormat: { bold: true },
+                                },
+                            },
+                            {
+                                userEnteredValue: {
+                                    stringValue: 'Relationships',
+                                },
+                                userEnteredFormat: {
+                                    textFormat: { bold: true },
+                                },
+                            },
+                            {
+                                userEnteredValue: {
+                                    stringValue: 'Tags',
+                                },
+                                userEnteredFormat: {
+                                    textFormat: { bold: true },
+                                },
+                            },
+                            {
+                                userEnteredValue: {
+                                    stringValue: 'Description',
+                                },
+                                userEnteredFormat: {
+                                    textFormat: { bold: true },
+                                },
+                            },
+                            {
+                                userEnteredValue: {
+                                    stringValue: 'Word Count',
+                                },
+                                userEnteredFormat: {
+                                    textFormat: { bold: true },
+                                },
+                            },
+                            {
+                                userEnteredValue: {
+                                    stringValue: 'Chapter Count',
+                                },
+                                userEnteredFormat: {
+                                    textFormat: { bold: true },
+                                },
+                            },
+                            {
+                                userEnteredValue: { 
+                                    stringValue: 'Status' 
+                                },
+                                userEnteredFormat: {
+                                    textFormat: { bold: true },
+                                },
+                            },
+                            {
+                                userEnteredValue: {
+                                    stringValue: 'History',
+                                },
+                                userEnteredFormat: {
+                                    textFormat: { bold: true },
+                                },
+                            },
+                            {
+                                userEnteredValue: {
+                                    stringValue: 'Personal Tags',
+                                },
+                                userEnteredFormat: {
+                                    textFormat: { bold: true },
+                                },
+                            },
+                            {
+                                userEnteredValue: {
+                                    stringValue: 'Rating',
+                                },
+                                userEnteredFormat: {
+                                    textFormat: { bold: true },
+                                },
+                            },
+                            {
+                                userEnteredValue: {
+                                    stringValue: 'Read Count',
+                                },
+                                userEnteredFormat: {
+                                    textFormat: { bold: true },
+                                },
+                            },
+                            {
+                                userEnteredValue: {
+                                    stringValue: 'Skip Reason',
+                                },
+                                userEnteredFormat: {
+                                    textFormat: { bold: true },
+                                },
+                            },
+                        ],
+                    },
+                    {
+                        values: [
+                            { userEnteredFormat: { wrapStrategy: 'WRAP' } },
+                            { userEnteredFormat: { wrapStrategy: 'WRAP' } },
+                            { userEnteredFormat: { wrapStrategy: 'WRAP' } },
+                            { userEnteredFormat: { wrapStrategy: 'WRAP' } },
+                            { userEnteredFormat: { wrapStrategy: 'WRAP' } },
+                            { userEnteredFormat: { wrapStrategy: 'WRAP' } },
+                            { userEnteredFormat: { wrapStrategy: 'WRAP' } },
+                            { userEnteredFormat: { wrapStrategy: 'WRAP' } },
+                            { userEnteredFormat: { wrapStrategy: 'WRAP' } },
+                            { userEnteredFormat: { wrapStrategy: 'WRAP' } },
+                            { userEnteredFormat: { wrapStrategy: 'WRAP' } },
+                            { userEnteredFormat: { wrapStrategy: 'WRAP' } },
+                            { userEnteredFormat: { wrapStrategy: 'WRAP' } },
+                            { userEnteredFormat: { wrapStrategy: 'WRAP' } },
+                            { userEnteredFormat: { wrapStrategy: 'WRAP' } },
+                            { userEnteredFormat: { wrapStrategy: 'WRAP' } },
+                        ]
+                    }
+                ],
+            },
+            { startColumn: 1, columnMetadata: { pixelSize: 100 } }, //work ID
+            { startColumn: 2, columnMetadata: { pixelSize: 200 } }, //title
+            { startColumn: 3, columnMetadata: { pixelSize: 200 } }, //authors
+            { startColumn: 4, columnMetadata: { pixelSize: 200 } }, //fandoms
+            { startColumn: 5, columnMetadata: { pixelSize: 200 } }, //relationships
+            { startColumn: 6, columnMetadata: { pixelSize: 200 } }, //tags
+            { startColumn: 7, columnMetadata: { pixelSize: 300 } }, //description
+            { startColumn: 8, columnMetadata: { pixelSize: 100 } }, //word count
+            { startColumn: 9, columnMetadata: { pixelSize: 100 } }, //chapter count
+            { startColumn: 10, columnMetadata: { pixelSize: 100 } }, //status
+            { startColumn: 11, columnMetadata: { pixelSize: 300 } }, //history
+            { startColumn: 12, columnMetadata: { pixelSize: 200 } }, //personal tags
+            { startColumn: 13, columnMetadata: { pixelSize: 100 } }, //rating
+            { startColumn: 14, columnMetadata: { pixelSize: 100 } }, //read count
+            { startColumn: 15, columnMetadata: { pixelSize: 200 } }, //skip reason
+        ],
+    },
+};
