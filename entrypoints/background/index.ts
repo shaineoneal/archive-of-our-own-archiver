fc14bae9cc764e882efe2a2c059849d7284419c5
Shaine ONeal <shaineoneal@gmail.com>
2025-02-09 20:22:04 -0500

    refactor(content-scripts): update structure for wxt compatibility

--- entrypoints/background/index.ts
+++ entrypoints/background/index.ts
@@ -1,5 +1,47 @@
 import { log } from "@/utils/logger.ts";
+import { handleTokenExchange, main as bgMain } from "./background.ts";
+import { createMessageHandlers, isAccessTokenValid, MessageName } from "@/utils/chrome-services";
+import { MessageResponse } from "@/utils/types/MessageResponse";
+import { SyncUserStore } from "@/utils/zustand";
+import { setAccessTokenCookie } from "@/utils/chrome-services/cookies.ts";
 
 export default defineBackground(() => {
-        log('background script running');
+    log('background script running');
+    let syncUser = SyncUserStore.getState().user;
+    createMessageHandlers({
+        // Called when the popup is opened
+        [MessageName.CheckLogin]: async (): Promise<MessageResponse<boolean>> => {
+            log('checkLogin message received');
+            const {setAccessToken} = SyncUserStore.getState().actions;
+            log('syncUser', syncUser);
+            if (syncUser.isLoggedIn && syncUser.refreshToken) {
+                log('user is logged in');
+                //setStore('user', syncUser, StoreMethod.SYNC);
+                try {
+                    if (syncUser.accessToken === undefined) {
+                        log('no access token');
+                        return {response: false};//return await handleTokenExchange<boolean>(syncUser.refreshToken);
+                    }
+                    const isValid = await isAccessTokenValid(syncUser.accessToken);
+                    log('is access token valid', isValid);
+                    if (isValid) {
+                        setAccessToken(syncUser.accessToken);
+                        await setAccessTokenCookie(syncUser.accessToken);
+                        log('newAccessToken set');
+                        return {response: true};
+                    } else {
+                        log('access token is not valid');
+                        return {response: false};//await handleTokenExchange<boolean>(syncUser.refreshToken);
+                    }
+                } catch (error) {
+                    log(error);
+                    return {response: false};
+                }
+            } else {
+                log('user is not logged in');
+                return {response: false};
+            }
+            },
+    });
+
 });
--- wxt.config.ts
+++ wxt.config.ts
@@ -1,36 +1,35 @@
-import { defineConfig } from 'wxt';
+import { defineConfig, defineRunnerConfig } from 'wxt';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  extensionApi: 'chrome',
-  modules: ['@wxt-dev/module-react'],
-  manifest: {
-    name: 'Rewritten AO3 Extension',
-    description: 'rewrite of AO3 Extension',
-    oauth2: {
-      client_id: '72283065139-0vv4km66vsale3ibeu2dr253gki1r8ln.apps.googleusercontent.com',
-      scopes: ['https://www.googleapis.com/auth/spreadsheets',
-        'https://www.googleapis.com/auth/userinfo.profile']
-    },
-    key: '***REMOVED***',
-    permissions: ['webRequest', 'identity', 'storage', 'scripting', 'identity.email', 'activeTab'],
-    host_permissions: ['*://*.archiveofourown.org/*', 'https://docs.google.com/spreadsheets/*', '*://*.google.com/'],
-    options_ui: {
-      page: "options.html",
-      open_in_tab: false
-    },
-    action: {
-      default_icon: 'icons/icon-16.png',
-      default_popup: 'entrypoints/popup/index.html'
-    },
-    browser_action: {
-        default_icon: 'icons/icon-16.png',
-        default_popup: 'index.html'
-    },
-    web_accessible_resources: [{
-      resources: ['js/content_script.css'],
-      matches: ['*://*.archiveofourown.org/*']
-    }]
-  }
+    extensionApi: 'chrome',
+    modules: ['@wxt-dev/module-react'],
+    manifest: {
+        name: 'Rewritten AO3 Extension',
+        description: 'rewrite of AO3 Extension',
+        oauth2: {
+            client_id: '72283065139-0vv4km66vsale3ibeu2dr253gki1r8ln.apps.googleusercontent.com',
+            scopes: ['https://www.googleapis.com/auth/spreadsheets',
+                'https://www.googleapis.com/auth/userinfo.profile']
+        },
+        key: '***REMOVED***',
+        permissions: ['webRequest', 'identity', 'storage', 'scripting', 'identity.email', 'activeTab'],
+        host_permissions: ['*://*.archiveofourown.org/*', 'https://docs.google.com/spreadsheets/*', '*://*.google.com/'],
+        options_ui: {
+            page: "options.html",
+            open_in_tab: false
+        },
+        action: {
+            default_icon: 'icons/icon-16.png',
+            default_popup: 'entrypoints/popup/index.html'
+        },
+        browser_action: {
+            default_icon: 'icons/icon-16.png',
+            default_popup: 'index.html'
+        },
+        web_accessible_resources: [{
+            resources: ['js/content_script.css'],
+            matches: ['*://*.archiveofourown.org/*']
+        }]
+    }
 });
-
