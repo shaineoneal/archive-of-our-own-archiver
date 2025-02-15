712c27ec39f4a9d97eac2f1db47295520eeca5a6
Shaine ONeal <shaineoneal@gmail.com>
2025-02-15 10:51:44 -0500

    fixed formatting for messaging protocol maps

--- utils/browser-services/messaging.ts
+++ utils/browser-services/messaging.ts
@@ -2,10 +2,11 @@ import { defineExtensionMessaging } from "@webext-core/messaging";
 import { Ao3_BaseWork, User_BaseWork } from "@/entrypoints/content";
 
 interface ProtocolMap {
-    addWorkToSpreadsheet(work: Ao3_BaseWork): User_BaseWork;
-    checkLogin(): boolean;
-    loggedIn(data: { accessToken: string, refreshToken: string }): void;
-    querySpreadSheet(searchList: number[]): boolean[];
+    AddWorkToSpreadsheet(work: Ao3_BaseWork): User_BaseWork;
+    CheckLogin(): boolean;
+    LoggedIn(data: { accessToken: string, refreshToken: string }): void;
+    QuerySpreadSheet(searchList: number[]): boolean[];
 }
 
 export const { sendMessage, onMessage } = defineExtensionMessaging<ProtocolMap>();
+
