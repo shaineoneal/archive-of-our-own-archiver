4d627932f4b8a039cf4cd34ef614f65fc07b9c14
Shaine ONeal <shaineoneal@gmail.com>
2025-03-08 19:47:07 -0500

    removed setAccessLevel since we are using local storage now

--- entrypoints/background/index.ts
+++ entrypoints/background/index.ts
@@ -8,13 +8,6 @@ import {
 export default defineBackground(() => {
     console.log('background script running');
 
-    if (import.meta.env.BROWSER === 'chrome') {
-        chrome.storage.session.setAccessLevel({accessLevel: 'TRUSTED_AND_UNTRUSTED_CONTEXTS'})
-            .then(() => {
-                console.log('access level set');
-            });
-    }
-
     onMessage('QuerySpreadSheet', handleQuerySpreadSheet);
     onMessage('AddWorkToSpreadsheet', handleAddWorkToSpreadsheet);
     onMessage('IsAccessTokenValid', handleIsAccessTokenValid);
