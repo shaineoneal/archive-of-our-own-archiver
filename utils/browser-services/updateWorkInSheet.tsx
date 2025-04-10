666451fb1e5a09ce916121dea48d847a9f7480a2
Shaine ONeal <shaineoneal@gmail.com>
2025-04-10 16:04:43 -0400

    forgot to update updateWorkInSheet.tsx to use Work instead of UserBasework

--- utils/browser-services/updateWorkInSheet.tsx
+++ utils/browser-services/updateWorkInSheet.tsx
@@ -1,7 +1,7 @@
-import { User_BaseWork } from "@/entrypoints/content";
+import { Work } from "@/entrypoints/content";
 import { HttpMethod, makeRequest } from './httpRequest.ts';
 
-export const updateWorkInSheet = async (spreadsheetId: string, accessToken: string, work: User_BaseWork): Promise<boolean> => {
+export const updateWorkInSheet = async (spreadsheetId: string, accessToken: string, work: Work): Promise<boolean> => {
     console.log('updateWorkInSheet', work);
 
     const response = await makeRequest({
@@ -14,16 +14,16 @@ export const updateWorkInSheet = async (spreadsheetId: string, accessToken: stri
         body: {
             valueInputOption: 'USER_ENTERED',
             data: [ {
-                range: `AccessWorks!K${work.index}`,
+                range: `AccessWorks!K${work.info?.index}`,
                 majorDimension: 'ROWS',
                 values: [
                     [
                         'read', // TODO: change this to a variable
-                        JSON.stringify(work.history),
-                        work.personalTags?.toString() ?? '',
-                        work.rating,
-                        work.readCount,
-                        work.skipReason?.toString() ?? ''
+                        JSON.stringify(work.info?.history),
+                        work.info?.personalTags?.toString() ?? '',
+                        work.info?.rating,
+                        work.info?.readCount,
+                        work.info?.skipReason?.toString() ?? ''
                     ]
                 ]
             }]
