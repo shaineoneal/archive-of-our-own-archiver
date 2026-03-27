7f07b23da07704ae3211d42321878ae50680fbdc
Shaine O'Neal <shaineoneal@gmail.com>
2026-03-27 14:13:58 -0500

    refactor: reorganize files and update import paths

---
+++ src/components/Popup/NewSheetButton.tsx
@@ -0,0 +1,47 @@
+import { createSpreadsheet } from '@/services';
+import { SyncUserStore, useLoaderStore } from '@/stores';
+
+/**
+ * Component for creating a new Google Sheet.
+ * Allows users to create a new spreadsheet and store its URL in Chrome's sync storage.
+ * Displays a loader while the spreadsheet is being created.
+ * @component
+ * @group Popup
+ * @returns the NewSheetButton component
+ */
+export const NewSheetButton = () => {
+    const { loader, setLoader } = useLoaderStore();
+    let { accessToken, spreadsheetId } = SyncUserStore.getState().user;
+    const setSpreadsheetId = SyncUserStore.getState().actions.setSpreadsheetId;
+
+    if (accessToken === undefined) {
+        return null;
+    }
+
+
+    /**
+     * Handles the creation of a new Google Sheet.
+     * Sets the loader state to true while the spreadsheet is being created.
+     * Stores the spreadsheet URL in Chrome's sync storage.
+     * updates the url in the options page.
+     */
+    const handleNewSheet = async () => {
+        setLoader(true);
+        const id = await createSpreadsheet(accessToken)
+        if (id) {
+            setSpreadsheetId(id);
+        }
+        setLoader(false);
+    };
+
+    return (
+        <div>
+            <button
+                id="new-sheet-button"
+                onClick={ handleNewSheet }
+            >
+                New Sheet
+            </button>
+        </div>
+    );
+};
