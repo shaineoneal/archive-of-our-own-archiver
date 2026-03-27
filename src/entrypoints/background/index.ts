ad7540357a1005772d4e0fa964c3dfcb45cf53f3
Shaine O'Neal <shaineoneal@gmail.com>
2026-03-27 14:13:58 -0500

    refactor: reorganize files and update import paths

---
+++ src/entrypoints/background/index.ts
@@ -0,0 +1,20 @@
+import {
+    handleAddWorkToSpreadsheet,
+    handleGetValidAccessToken,
+    handleIsAccessTokenValid,
+    handleLogin,
+    handleQuerySpreadSheet,
+    handleUpdateWorkInSpreadsheet,
+    onMessage
+} from "@/services";
+
+export default defineBackground(() => {
+    logger.info('background script running');
+
+    onMessage('QuerySpreadSheet', handleQuerySpreadSheet);
+    onMessage('AddWorkToSpreadsheet', handleAddWorkToSpreadsheet);
+    onMessage('IsAccessTokenValid', handleIsAccessTokenValid);
+    onMessage('GetValidAccessToken', handleGetValidAccessToken);
+    onMessage('Login', handleLogin);
+    onMessage('UpdateWorkInSpreadsheet', handleUpdateWorkInSpreadsheet);
+});
