12df63811b4664f117d0910c266b20ede60a1224
Shaine O'Neal <shaineoneal@gmail.com>
2026-04-12 19:20:34 -0500

    chore: update build configuration

--- web-ext.config.ts
+++ web-ext.config.ts
@@ -3,7 +3,7 @@ import { resolve } from 'node:path';
 
 export default defineWebExtConfig({
     openDevtools: true,
-    chromiumProfile: resolve(import.meta.env.WXT_API_PROFILE_ADDRESS),
+    chromiumProfile: import.meta.env.WXT_API_PROFILE_ADDRESS ? resolve(import.meta.env.WXT_API_PROFILE_ADDRESS) : undefined,
     keepProfileChanges: true,
     openConsole: true,
     startUrls: [ 'chrome://extensions', 'https://archiveofourown.org/works/'],
